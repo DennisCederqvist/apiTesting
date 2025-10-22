62e462195a6d50002ba9c09bc50cb12a1f2c47ac
Dennis.C <dennis.cederqvist@chasacademy.se>
2025-10-22 10:09:54 +0200

    Created the dad-joke-generator

---
+++ dad-joke-generator/app.js
@@ -0,0 +1,21 @@
+
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+
+// Hoisting!
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    };
+    fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+        jokeElement.innerHTML = data.joke;
+    });
+}
+
+jokeBtn.addEventListener('click', generateJoke);
