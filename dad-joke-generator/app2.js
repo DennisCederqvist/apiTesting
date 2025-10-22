2d5281de1c68369ca8e1567180dae68c83b8e175
Dennis.C <dennis.cederqvist@chasacademy.se>
2025-10-22 10:58:56 +0200

    made an async version 2 times for practice

--- dad-joke-generator/app2.js
+++ dad-joke-generator/app2.js
@@ -3,18 +3,15 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-// Hoisting!
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeElement.innerHTML = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeElement.innerHTML = data.joke;
 }
 
 jokeBtn.addEventListener("click", generateJoke);
